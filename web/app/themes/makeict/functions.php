51e9bc8dfa757808b27db934b4b7614e8a0ea369
Dominic Canare <dom@makeict.org>
2016-05-30 06:02:37 -0500

    make menus work

--- web/app/themes/makeict/functions.php
+++ web/app/themes/makeict/functions.php
@@ -1,19 +1,31 @@
 <?php
-/*
-add_action( 'init', 'create_project_posttype' );
 
-function create_project_posttype() {
-  register_post_type( 'project',
-    array(
-      'labels' => array(
-        'name' => __( 'Projects' ),
-        'singular_name' => __( 'Project' )
-      ),
-      'public' => true,
-      'menu_position' => 5,
-      'has_archive' => false,
-      'rewrite' => array('slug' => 'projects'),
-    )
-  );
+function register_my_menu() {
+	register_nav_menu('header-menu',__( 'Header Menu' ));
 }
-*/
+add_action( 'init', 'register_my_menu' );
+
+class MakeICT_menu_walker extends Walker_Nav_Menu {
+    function start_lvl( &$output, $depth = 0, $args = array() ) {
+    }
+    
+    function end_lvl( &$output, $depth = 0, $args = array() ) {
+	}
+ 
+    function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+        if($depth == 0){
+			$output .= "<li class=\"dropdown\">\n";
+			$output .= '	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">' . $item->title . '<span class="caret"></span></a>'. "\n";
+			$output .= "	<ul class=\"dropdown-menu\">\n";
+		}else{
+			$output .= "		<li><a href=\"/" . $item->url . "\">" . $item->title . "</a></li>\n";
+		}
+    }
+    
+	function end_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+		if($depth == 0){
+			$output .= "	</ul>\n";
+			$output .= "</li>\n";
+		}
+   }    
+}
--- web/app/themes/makeict/header.php
+++ web/app/themes/makeict/header.php
@@ -15,7 +15,6 @@
       <title><?php echo get_bloginfo('name'); ?><?php wp_title('|'); ?></title>
     <?php } ?>
 
-
     <!-- Bootstrap core CSS -->
     <link href="<?php echo get_template_directory_uri(); ?>/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
@@ -42,64 +41,15 @@
           </button>
           <a class="navbar-brand" href="/"><?php echo bloginfo('name'); ?></a>
         </div>
-        <div id="navbar" class="navbar-collapse collapse pull-right">
-
-
-
-  <ul class="nav navbar-nav">
-	<li class="dropdown">
-		<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Membership<span class="caret"></span></a>
-		<ul class="dropdown-menu">
-			<li><a href="/join">How to join</a></li>
-			<li><a href="/membership-benefits">Benefits</a></li>
-			<li><a href="/rules-and-policies">Rules and policies</a></li>
-			<li><a href="/membership">Manage my membership</a></li>
-			<li><a href="/reservations">Reserve space</a></li>
-			<li><a href="/wiki">The Wiki</a></li>
-		</ul>
-	</li>
-	<li class="dropdown">
-		<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Events<span class="caret"></span></a>
-		<ul class="dropdown-menu">
-			<li><a href="/maker-mondays">Maker Mondays</a></li>
-			<li><a href="/orientation">New member orientation</a></li>
-			<li><a href="/calendar">Calendar and event list</a></li>
-			<li><a href="/education">Suggest an event or workshop</a></li>
-		</ul>
-	</li>
-	<li class="dropdown">
-		<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Facilities &amp; Programs<span class="caret"></span></a>
-		<ul class="dropdown-menu">
-			<li><a href="/makerspace-facilities">Makerspace</a></li>
-			<li><a href="/devICT">devICT</a></li>
-			<li><a href="/education">Education</a></li>
-			<li><a href="/outreach">Outreach</a></li>
-		</ul>
-	</li>
-	<li class="dropdown">
-		<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Contribute<span class="caret"></span></a>
-		<ul class="dropdown-menu">
-			<li><a href="/donate">Donate</a></li>
-			<li><a href="/education">Teach</a></li>
-			<li><a href="/volunteer">Volunteer</a></li>
-		</ul>
-	</li>
-	<li class="dropdown">
-		<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Community<span class="caret"></span></a>
-		<ul class="dropdown-menu">
-			<li><a href="/forum">Forum</a></li>
-			<li><a href="/featured-makers">Featured makers</a></li>
-			<li><a href="/team-projects">Team Projects</a></li>
-			<li><a href="/leadership">Leadership</a></li>
-			<li><a href="/newsletter">Newsletter</a></li>
-		</ul>
-	</li>
-</ul>
-
-
-         
-
-
-        </div><!--/.navbar-collapse -->
+        
+		<?php
+			$args = array(
+				'theme_location' => 'header-menu',
+				'menu_class' => 'nav navbar-nav',
+				'container_class' => 'navbar-collapse collapse pull-right',
+				'walker' => new MakeICT_menu_walker()
+			);
+			wp_nav_menu($args); 
+		?>
       </div>
     </nav>
