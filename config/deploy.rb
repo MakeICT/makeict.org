985c645bd79f94ce539b6723bf73c6e03586d2f6
Dominic Canare <dom@makeict.org>
2016-05-30 06:02:02 -0500

    set deployment parameters

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, 'makeict.org'
-set :repo_url, 'git@github.com:devict/makeict.org.git'
+set :repo_url, 'git@github.com:makeict/makeict.org.git'
 
 # Branch options
 # Prompts for the branch name (defaults to current branch)
@@ -9,7 +9,7 @@ set :repo_url, 'git@github.com:devict/makeict.org.git'
 # This could be overridden in a stage config file
 set :branch, :master
 
-set :deploy_to, -> { "/var/www/#{fetch(:application)}" }
+set :deploy_to, -> { "/home/makeict/sites/#{fetch(:application)}" }
 
 # Use :debug for more verbose output when troubleshooting
 set :log_level, :info
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
 set :stage, :production
+set :tmp_dir, "/home/makeict/tmp"
 
 # Simple Role Syntax
 # ==================
@@ -8,17 +9,7 @@ set :stage, :production
 
 # Extended Server Syntax
 # ======================
-server 'example.com', user: 'deploy', roles: %w{web app db}
-
-# you can set custom ssh options
-# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
-# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
-# set it globally
-#  set :ssh_options, {
-#    keys: %w(~/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+server 'makeict.org', user: 'makeict', roles: %w{web app db}
 
 fetch(:default_env).merge!(wp_env: :production)
 
