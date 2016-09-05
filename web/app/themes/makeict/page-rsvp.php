cd0de67bd56f9b72734aeea0d129124ad5025f47
Dominic Canare <dom@makeict.org>
2016-09-05 15:21:31 -0500

    change top level headings and spacing

--- web/app/themes/makeict/page-rsvp.php
+++ web/app/themes/makeict/page-rsvp.php
@@ -8,11 +8,12 @@
 <?php get_header(); ?>
 
 <!-- Main jumbotron for a primary marketing message or call to action -->
-<div class="jumbotron">
+<div class="jumbotron jumbotron-main">
   <div class="container">
     <h1><?php the_title(); ?></h1>
   </div>
 </div>
+<div class="jumbotron-spacer"></div>
 
 <div class="container">
   <!-- Example row of columns -->
