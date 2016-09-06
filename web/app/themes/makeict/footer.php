67a978685dbf1cc3ed613c508a3a002645528f01
Dominic Canare <dom@makeict.org>
2016-09-05 19:16:33 -0500

    add social media icons

--- web/app/themes/makeict/footer.php
+++ web/app/themes/makeict/footer.php
@@ -5,9 +5,16 @@
 		  MakeICT is a 501(c)(3) charitable nonprofit that operates the makerspace at<br/>
 		  <a href="https://www.google.com/maps/place/1500+E+Douglas+Ave,+Wichita,+KS+67214/" target="_blank">1500 E Douglas Ave in Wichita, Kansas</a>
 		</p>
+		<hr/>
+		<p>
+			<a href="https://facebook.com/makeict"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-facebook.png" width="20" height="20" alt="Facebook"></a>
+			<a href="https://twitter.com/make_ict"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-twitter.png" width="20" height="20" alt="Twitter"></a>
+			<a href="https://meetup.com/makeict"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-meetup.png" width="20" height="20"></a>
+			<a href="https://www.youtube.com/channel/UCkbGNG0Nf_vpW5QtuRzPLsw"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-youtube.png" width="20" height="20"></a>
+			<a href="https://github.com/makeict"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-github.png" width="20" height="20"></a>
+		</p>
       </footer>
     </div> <!-- /container -->
-
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
